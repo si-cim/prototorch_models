a9382dcd9be5c0f38a8a0c9ed96520833a08dcf9
Jensun Ravichandran <jjensun@gmail.com>
2021-05-21 13:11:48 +0200

    Add get_prototype_grid method

--- prototorch/models/abstract.py
+++ prototorch/models/abstract.py
@@ -31,6 +31,13 @@ class PrototypeImageModel(pl.LightningModule):
     def on_train_batch_end(self, outputs, batch, batch_idx, dataloader_idx):
         self.proto_layer.components.data.clamp_(0.0, 1.0)
 
+    def get_prototype_grid(self, nrow=2, return_channels_last=True):
+        from torchvision.utils import make_grid
+        grid = make_grid(self.components, nrow=nrow)
+        if return_channels_last:
+            grid = grid.permute((1, 2, 0))
+        return grid.cpu()
+
 
 class SiamesePrototypeModel(pl.LightningModule):
     def configure_optimizers(self):
