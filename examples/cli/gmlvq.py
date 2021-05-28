dade502686a4ab88328612b6b46b3afaec3d8f3b
Alexander Engelsberger <aengelsb@hs-mittweida.de>
2021-05-28 16:33:31 +0200

    Add MNIST datamodule and training mixin factory.

--- examples/cli/gmlvq.py
+++ examples/cli/gmlvq.py
@@ -1,12 +1,11 @@
 """GLVQ example using the MNIST dataset."""
 
 from prototorch.models import ImageGLVQ
+from prototorch.models.data import train_on_mnist
 from pytorch_lightning.utilities.cli import LightningCLI
 
-from mnist import TrainOnMNIST
 
-
-class GLVQMNIST(TrainOnMNIST, ImageGLVQ):
+class GLVQMNIST(train_on_mnist(batch_size=64), ImageGLVQ):
     """Model Definition."""
 
 
