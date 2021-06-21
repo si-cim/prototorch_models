2649e3ac310741366e7ca787cada3d2c838811e4
Alexander Engelsberger <alexanderengelsberger@gmail.com>
2021-06-21 15:06:37 +0200

    test(examples): print error message on fail

--- tests/test_examples.sh
+++ tests/test_examples.sh
@@ -5,13 +5,15 @@ failed=0
 for example in $(find $1 -maxdepth 1 -name "*.py")
 do
     echo  -n "$x" $example '... '
-    export DISPLAY= && python $example --fast_dev_run 1 &> /dev/null
+    export DISPLAY= && python $example --fast_dev_run 1 &> run_log.txt
     if [[ $? -ne 0 ]]; then
         echo "FAILED!!"
+        cat run_log.txt
         failed=1
     else
         echo "SUCCESS!"
     fi
+    rm run_log.txt
 done
 
 exit $failed
