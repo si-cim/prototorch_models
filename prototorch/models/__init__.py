626f51ce8046a66b7bceb95cdc9ecf46a2c66fa9
Alexander Engelsberger <engelsbe@hs-mittweida.de>
2022-06-03 10:46:30 +0200

    ci: Add possible prerelease to bumpversion

--- prototorch/models/__init__.py
+++ prototorch/models/__init__.py
@@ -1,39 +1,3 @@
 """`models` plugin for the `prototorch` package."""
 
-from .callbacks import PrototypeConvergence, PruneLoserPrototypes
-from .cbc import CBC, ImageCBC
-from .glvq import (
-    GLVQ,
-    GLVQ1,
-    GLVQ21,
-    GMLVQ,
-    GRLVQ,
-    GTLVQ,
-    LGMLVQ,
-    LVQMLN,
-    ImageGLVQ,
-    ImageGMLVQ,
-    ImageGTLVQ,
-    SiameseGLVQ,
-    SiameseGMLVQ,
-    SiameseGTLVQ,
-)
-from .knn import KNN
-from .lvq import (
-    LVQ1,
-    LVQ21,
-    MedianLVQ,
-)
-from .probabilistic import (
-    CELVQ,
-    RSLVQ,
-    SLVQ,
-)
-from .unsupervised import (
-    GrowingNeuralGas,
-    KohonenSOM,
-    NeuralGas,
-)
-from .vis import *
-
 __version__ = "0.5.2"
