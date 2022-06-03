48e7c029fa81f3213b6ab6443ff3f1fb3c6c076d
Alexander Engelsberger <engelsbe@hs-mittweida.de>
2022-06-03 11:40:45 +0200

    fix: Fix __init__.py

--- prototorch/models/__init__.py
+++ prototorch/models/__init__.py
@@ -1,3 +1,39 @@
 """`models` plugin for the `prototorch` package."""
 
+from .callbacks import PrototypeConvergence, PruneLoserPrototypes
+from .cbc import CBC, ImageCBC
+from .glvq import (
+    GLVQ,
+    GLVQ1,
+    GLVQ21,
+    GMLVQ,
+    GRLVQ,
+    GTLVQ,
+    LGMLVQ,
+    LVQMLN,
+    ImageGLVQ,
+    ImageGMLVQ,
+    ImageGTLVQ,
+    SiameseGLVQ,
+    SiameseGMLVQ,
+    SiameseGTLVQ,
+)
+from .knn import KNN
+from .lvq import (
+    LVQ1,
+    LVQ21,
+    MedianLVQ,
+)
+from .probabilistic import (
+    CELVQ,
+    RSLVQ,
+    SLVQ,
+)
+from .unsupervised import (
+    GrowingNeuralGas,
+    KohonenSOM,
+    NeuralGas,
+)
+from .vis import *
+
 __version__ = "1.0.0-a1"
