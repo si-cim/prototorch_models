696719600b8c119c93306285f0614adb87a6788d
Alexander Engelsberger <engelsbe@hs-mittweida.de>
2022-06-03 11:52:50 +0200

    build: bump version 1.0.0a1 → 1.0.0a2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Jensun Ravichandran"
 
 # The full version, including alpha/beta/rc tags
 #
-release = "1.0.0-a1"
+release = "1.0.0-a2"
 
 # -- General configuration ---------------------------------------------------
 
--- prototorch/models/__init__.py
+++ prototorch/models/__init__.py
@@ -36,4 +36,4 @@ from .unsupervised import (
 )
 from .vis import *
 
-__version__ = "1.0.0-a1"
+__version__ = "1.0.0-a2"
