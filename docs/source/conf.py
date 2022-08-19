c3cad198535ebfbf883a31dc34d36e75e00b69a9
Alexander Engelsberger <engelsbe@hs-mittweida.de>
2022-08-19 12:17:32 +0200

    build: bump version 1.0.0a6 → 1.0.0a7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Jensun Ravichandran"
 
 # The full version, including alpha/beta/rc tags
 #
-release = "1.0.0-a6"
+release = "1.0.0-a7"
 
 # -- General configuration ---------------------------------------------------
 
