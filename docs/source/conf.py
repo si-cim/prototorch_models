8ecf9948b2aa5e29ce6b2ae7d23f7746e97a1e5c
Alexander Engelsberger <engelsbe@hs-mittweida.de>
2023-03-02 17:24:11 +0000

    build: bump version 0.5.2 → 0.5.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Jensun Ravichandran"
 
 # The full version, including alpha/beta/rc tags
 #
-release = "0.5.2"
+release = "0.5.3"
 
 # -- General configuration ---------------------------------------------------
 
